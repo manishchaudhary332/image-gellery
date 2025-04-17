033b0cde044c674f29decddeb1b9cedbcd3f8d43
Manish Chaudhary <manishchaudhary3803@gmail.com>
2025-04-17 23:34:12 +0530

    some style

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -6,10 +6,11 @@ const ImageCard = ({ image }) => {
           src={image.urls.small}
           alt={image.alt_description}
         />
-        <div className="px-5 py-4">
-          <h2 className="font-semibold text-xl text-gray-800 mb-2">{image.user.name}</h2>
-          <p className="text-gray-500 text-sm">{image.alt_description || "Beautiful image"}</p>
-        </div>
+        <div className="px-5 py-4 bg-zinc-200 h-[100px] flex flex-col ">
+           <h2 className="font-semibold text-xl text-gray-800">{image.user.name}</h2>
+           <p className="text-gray-500 text-sm">{image.alt_description || "Beautiful image"}</p>
+       </div>
+
       </div>
     );
   };
